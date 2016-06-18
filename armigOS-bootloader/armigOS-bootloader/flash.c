dfdd650b1787f78952dc71691d27f8861918ec7e
minedev <jytho@web.de>
2016-06-18 03:18:12 +0200

    rename flash method

--- armigOS-bootloader/armigOS-bootloader/flash.c
+++ armigOS-bootloader/armigOS-bootloader/flash.c
@@ -1,9 +1,9 @@
+#include "flash.h"
 #include <inttypes.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
-#include <avr/boot.h>
 
-void boot_program_page(uint32_t page, uint8_t *buffer)
+void flash_program_page(uint32_t page, uint8_t *buffer)
 {
 	uint16_t i;
 	uint8_t sreg;
--- armigOS-bootloader/armigOS-bootloader/flash.h
+++ armigOS-bootloader/armigOS-bootloader/flash.h
@@ -1,7 +1,9 @@
 #ifndef _FLASH_H
 #define _FLASH_H
 
+#include <avr/boot.h>
+
 // Write a buffer to a flash page
-void boot_program_page(uint32_t page, uint8_t *buffer);
+void flash_program_page(uint32_t page, uint8_t *buffer);
 
 #endif
